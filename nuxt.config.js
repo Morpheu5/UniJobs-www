a850399d4564b7ec97da999ee6245c24d69b8849
Andrea Franceschini <andrea.franceschini@gmail.com>
2018-09-21 21:15:09 +0100

    :recycle: House cleaning

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   /* Headers of the page */
   head: {
-    title: 'Jobs',
+    title: 'UniJobs.it',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'jobs.ac.it' }
+      { hid: 'description', name: 'description', content: 'unijobs.it' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
